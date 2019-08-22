8986a83de7eb85ec06a2f5268cb61ad1c6aacd93
Andrew <barabym@yandex.ru>
2019-08-22 15:56:17 +0300

    Сохраняю перед чисткой

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,8 @@
 import { createStore } from 'redux';
+// import { createStore, applyMiddleware } from 'redux';
 import { rootReducer } from '../reducer';
 import { composeWithDevTools } from 'redux-devtools-extension';
+// import logger from 'redux-logger';
 
+// export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
 export const store = createStore(rootReducer, composeWithDevTools());
